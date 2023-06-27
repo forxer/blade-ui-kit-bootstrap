2091e3fe427373d40842b6e2635d71506662fae5
Vincent Garnier <forxer@gmail.com>
2023-06-27 08:44:46 +0200

    adapt all other component

--- src/Components/Forms/Inputs/Password.php
+++ src/Components/Forms/Inputs/Password.php
@@ -4,20 +4,15 @@ declare(strict_types=1);
 
 namespace BladeUIKitBootstrap\Components\Forms\Inputs;
 
-use BladeUIKitBootstrap\Concerns\HasBootstrapVersion;
-use Illuminate\Contracts\View\View;
-
 class Password extends Input
 {
-    use HasBootstrapVersion;
-
-    public function __construct(string $name, string $id = null, ?string $errorBag = null)
+    public function __construct(string $name = 'password', string $id = null, ?string $errorBag = null)
     {
         parent::__construct($name, $id, 'password', null, $errorBag);
     }
 
-    public function render(): View
+    public function viewName(): string
     {
-        return view($this->viewPath('components.forms.inputs.password'));
+        return 'components.forms.inputs.password';
     }
 }
--- src/Components/Forms/Inputs/Time.php
+++ src/Components/Forms/Inputs/Time.php
@@ -4,20 +4,15 @@ declare(strict_types=1);
 
 namespace BladeUIKitBootstrap\Components\Forms\Inputs;
 
-use BladeUIKitBootstrap\Concerns\HasBootstrapVersion;
-use Illuminate\Contracts\View\View;
-
 class Time extends Input
 {
-    use HasBootstrapVersion;
-
     public function __construct(string $name, string $id = null, ?string $value = '', ?string $errorBag = null)
     {
         parent::__construct($name, $id, 'time', $value, $errorBag);
     }
 
-    public function render(): View
+    public function viewName(): string
     {
-        return view($this->viewPath('components.forms.inputs.time'));
+        return 'components.forms.inputs.time';
     }
 }
