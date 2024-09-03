d9bcaec5b01c9e715debfc9fdea64ae20b422c85
Vincent Garnier <forxer@gmail.com>
2024-09-03 10:09:36 +0200

    fix back buttons

--- src/Components/Buttons/Actions/Back/BackHome.php
+++ src/Components/Buttons/Actions/Back/BackHome.php
@@ -4,15 +4,20 @@ declare(strict_types=1);
 
 namespace BladeUIKitBootstrap\Components\Buttons\Actions\Back;
 
+use BladeUIKitBootstrap\Components\Buttons\LinkButton;
 use Illuminate\Support\Str;
 
-class BackHome extends Back
+class BackHome extends LinkButton
 {
     protected function initAttributes(): void
     {
         $this->variant ??= 'primary';
 
         $this->text ??= Str::ucfirst(trans('back.home'));
+
+        if ($this->confirm !== null && $this->confirmId === null) {
+            $this->confirmId = 'back-'.Str::random(32);
+        }
     }
 
     public function viewName(): string
--- src/Components/Buttons/Actions/Back/BackList.php
+++ src/Components/Buttons/Actions/Back/BackList.php
@@ -4,15 +4,20 @@ declare(strict_types=1);
 
 namespace BladeUIKitBootstrap\Components\Buttons\Actions\Back;
 
+use BladeUIKitBootstrap\Components\Buttons\LinkButton;
 use Illuminate\Support\Str;
 
-class BackList extends Back
+class BackList extends LinkButton
 {
     protected function initAttributes(): void
     {
         $this->variant ??= 'secondary';
 
         $this->text ??= Str::ucfirst(trans('back.list'));
+
+        if ($this->confirm !== null && $this->confirmId === null) {
+            $this->confirmId = 'back-'.Str::random(32);
+        }
     }
 
     public function viewName(): string
